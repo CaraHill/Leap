545e24d91f71ead4ffb720c2ecf4ffd0d4f76126
Cara Hill <cara@rabidtech.co.nz>
2018-10-28 20:42:59 +1300

    Refactored function.

--- leap.js
+++ leap.js
@@ -1,9 +1,17 @@
 function isLeap(year) {
-  if((year % 4 == 0 && year % 100 != 0) || (year % 100 != 0 && year % 400 == 0) || (year % 400 == 0)) {
-    return true;
-  } else {
-    return false;
-  }
+  return divisibleByFourNotHundred(year) || divisibleByHundredNotFourHundred(year) || divisibleByFourHundred(year);
+}
+
+function divisibleByFourNotHundred(year) {
+  return year % 4 == 0 && year % 100 != 0;
+}
+
+function divisibleByHundredNotFourHundred(year) {
+  return year % 100 != 0 && divisibleByFourHundred(year);
+}
+
+function divisibleByFourHundred(year) {
+  return year % 400 == 0;
 }
 
 export { isLeap }
